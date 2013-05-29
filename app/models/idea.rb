86851fc8796932f5a6c4f0b9c40647682fd2d20f
Max Claus Nunes <maxcnunes@gmail.com>
2013-05-28 21:22:45 -0300

    Improved layout crud ideas
    Improved layout 2

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,10 @@
 class Idea < ActiveRecord::Base
   attr_accessible :created_date, :description, :negative, :positive, :title
+
+  before_save :default_values
+  def default_values
+  	self.negative ||= 0
+  	self.positive ||= 0
+  	self.created_date ||= Time.now
+  end
 end
